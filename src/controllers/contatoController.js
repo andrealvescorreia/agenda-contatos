b64ed2966c864cbd2a81253ef29bef32aca97a34
André Alves Correia <58227057+andrealvescorreia@users.noreply.github.com>
2024-01-30 09:16:47 -0300

    clean up

--- src/controllers/contatoController.js
+++
@@ -1,3 +0,0 @@
-exports.paginaInicial = (req, res) => {
-  res.send('Pagina de contato')
-}
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,3 @@
-exports.paginaInicial = (req, res) => {
+exports.index = (req, res) => {
   res.render('index');
-  return;// fim (ultimo middleware)
-}
-
-exports.trataPost = (req, res) => {
-  res.send(res.locals);
-  return;// fim (ultimo middleware)
-}
+};
